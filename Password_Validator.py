5e88910b4e9d77c9a16c1b24e02324bb0a3db592
unknown <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2021-11-30 15:35:18 +0800

    Password_Validator_Colorama

--- Password_Validator.py
+++ Password_Validator.py
@@ -1,30 +1,33 @@
-print("Hello User, I'm KDNL Programming")
-print("\nCreate a password that contains:")
-print("Greater than 15 letters \nHave at least one capital letter \nHave at least one number \nHave at least one special char ")
+import colorama
+colorama.init 
 
-User_Password= input("\nPassword: ")
+print(colorama.Fore.BLUE + "Hello User, I'm KDNL Programming")
+print(colorama.Fore.CYAN + "\nCreate a password that contains:")
+print(colorama.Fore.YELLOW + "\nGreater than 15 letters \nHave at least one capital letter \nHave at least one number \nHave at least one special char ")
+
+User_Password= input(colorama.Fore.WHITE + "\nPassword: ")
 Password_value = True
 Character_Special=['!', '@', '#',"$", "%", "^", "&", "*", "(",")","_","+"]
 
 while Password_value:  
     if (len(User_Password)<15):
-        print("It must contain greater than 15 letters")
+        print(colorama.Fore.RED + "It must contain greater than 15 letters")
         Password_value=False
 
     if not any(char.isupper() for char in User_Password):
-        print("It must contain atleast one capital letter")
+        print(colorama.Fore.RED + "It must contain atleast one capital letter")
         Password_value= False
 
     if not any(char.isdigit() for char in User_Password):
-        print("It must contain atleast one number")
+        print(colorama.Fore.RED + "It must contain atleast one number")
         Password_value= False
 
     if not any(char in Character_Special for char in User_Password):
-        print("It must contain atleast one special character")
+        print(colorama.Fore.RED + "It must contain atleast one special character")
         Password_value= False
         
     if Password_value:
-        print("Your Password is Valid")
+        print(colorama.Fore.GREEN + "Your Password is Valid")
         break
    
         
