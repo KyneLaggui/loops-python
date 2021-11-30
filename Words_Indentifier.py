69a1686faf145072a68010238bff46a9d59c5f2f
unknown <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2021-11-30 14:04:14 +0800

    Words_Identifier_Comments_and_Color

--- Words_Indentifier.py
+++ Words_Indentifier.py
@@ -1,10 +1,16 @@
+import colorama
+colorama.init 
+
 User_Words=input("Input: ")
 
 User_Words_Vowels=0
 User_Words_Consonants=0
 
 Without_SC_Space = ''.join(filter(str.isalnum, User_Words.lower()))
+#Removes the Special Characters and Space. Automatically lowercase the uppercase letters. 
+
 User_Number_of_Words = len(User_Words.split())
+#Count the number of words
 
 for k in Without_SC_Space:
     if(k == 'a'or k == 'e'or k == 'i'or k == 'o'or k == 'u'):
@@ -12,6 +18,6 @@ for k in Without_SC_Space:
     else:
         User_Words_Consonants=User_Words_Consonants+1
 
-print(f"The number of words are {User_Number_of_Words}")
-print(f"The number of vowels are {User_Words_Vowels}")
-print(f"The number of consonants are {User_Words_Consonants}")
+print(colorama.Fore.GREEN + f"\nThe number of words are {User_Number_of_Words}")
+print(colorama.Fore.CYAN + f"The number of vowels are {User_Words_Vowels}")
+print(colorama.Fore.YELLOW + f"The number of consonants are {User_Words_Consonants}")
